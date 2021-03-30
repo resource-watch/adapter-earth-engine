5b243ee7873b6716103d99decb0602d37c8da443
Tiago Garcia <tiagojsag@gmail.com>
2021-03-30 16:09:04 +0200

    Modify API HTTP verbs to match public API method signature

--- tests/download_get_tests.py
+++ tests/download_get_tests.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 import requests_mock
+import json
 
 import adapterearthengine
 
@@ -15,17 +16,17 @@ def client():
 
 
 @requests_mock.Mocker(kw='mocker')
-def test_get_fields_for_dataset_that_does_not_exist(client, mocker):
+def test_download_dataset_that_does_not_exist(client, mocker):
     mocker.get('{}/v1/dataset/foo'.format(os.getenv('CT_URL')), status_code=404,
                json={"errors": [{"status": 404, "detail": "Dataset with id 'foo' doesn't exist"}]})
 
-    response = client.get('/api/v1/earthengine/fields/foo')
+    response = client.get('/api/v1/earthengine/download/foo')
     assert response.status_code == 404
     assert response.data == b'{"errors":[{"detail":"Dataset with id foo doesn\'t exist","status":404}]}\n'
 
 
 @requests_mock.Mocker(kw='mocker')
-def test_get_fields_for_dataset_invalid_provider(client, mocker):
+def test_download_dataset_invalid_provider(client, mocker):
     dataset_json = {
         'data': {
             'id': 'bar',
@@ -67,13 +68,13 @@ def test_get_fields_for_dataset_invalid_provider(client, mocker):
 
     mocker.get('{}/v1/dataset/bar'.format(os.getenv('CT_URL')), json=dataset_json)
 
-    response = client.get('/api/v1/earthengine/fields/bar')
+    response = client.get('/api/v1/earthengine/download/bar')
     assert response.status_code == 422
     assert response.data == b'{"errors":[{"detail":"This operation is only supported for datasets with provider \'gee\'","status":422}]}\n'
 
 
 @requests_mock.Mocker(kw='mocker')
-def test_get_fields_for_dataset_invalid_connector_type(client, mocker):
+def test_download_dataset_invalid_connector_type(client, mocker):
     dataset_json = {
         'data': {
             'id': 'bar',
@@ -115,13 +116,13 @@ def test_get_fields_for_dataset_invalid_connector_type(client, mocker):
 
     mocker.get('{}/v1/dataset/bar'.format(os.getenv('CT_URL')), json=dataset_json)
 
-    response = client.get('/api/v1/earthengine/fields/bar')
+    response = client.get('/api/v1/earthengine/download/bar')
     assert response.status_code == 422
     assert response.data == b'{"errors":[{"detail":"This operation is only supported for datasets with connectorType \'rest\'","status":422}]}\n'
 
 
 @requests_mock.Mocker(kw='mocker')
-def test_get_fields_for_dataset_happy_case(client, mocker):
+def test_download_dataset_no_download(client, mocker):
     dataset_json = {
         'data': {
             'id': 'bar',
@@ -161,21 +162,87 @@ def test_get_fields_for_dataset_happy_case(client, mocker):
             }
         }}
 
-    query_json = {
+    mocker.get('{}/v1/dataset/bar'.format(os.getenv('CT_URL')), json=dataset_json)
+
+    response = client.get('/api/v1/earthengine/download/bar')
+    assert response.status_code == 400
+    assert response.data == b'{"errors":[{"detail":"sql or fs not provided","status":400}]}\n'
+
+
+# Failing because of an issue with sql2gee
+@requests_mock.Mocker(kw='mocker')
+def test_download_dataset_happy_case(client, mocker):
+    dataset_json = {
+        'data': {
+            'id': 'bar',
+            'type': 'dataset',
+            'attributes': {
+                'name': 'Test dataset 1',
+                'slug': 'test-dataset-1',
+                'type': 'tabular',
+                'subtitle': None,
+                'application': [
+                    'rw'
+                ],
+                'dataPath': None,
+                'attributesPath': None,
+                'connectorType': 'rest',
+                'provider': 'gee',
+                'userId': '1',
+                'connectorUrl': 'https://raw.githubusercontent.com/test/file.csv',
+                'sources': [],
+                'tableName': 'CGIAR/SRTM90_V4',
+                'status': 'saved',
+                'published': False,
+                'overwrite': True,
+                'mainDateField': None,
+                'env': 'production',
+                'geoInfo': False,
+                'protected': False,
+                'clonedHost': {},
+                'legend': {},
+                'errorMessage': None,
+                'taskId': None,
+                'createdAt': '2016-08-01T15:28:15.050Z',
+                'updatedAt': '2018-01-05T18:15:23.266Z',
+                'dataLastUpdated': None,
+                'widgetRelevantProps': [],
+                'layerRelevantProps': []
+            }
+        }}
+
+    download_json = {
         "data": {
             "type": "result",
             "attributes": {
-                "query": "SELECT * FROM \"srtm90_v4\" LIMIT 1",
+                "query": "SELECT ST_HISTOGRAM(rast,elevation,10,true) FROM CGIAR/SRTM90_V4",
                 "jsonSql": {
                     "select": [
                         {
-                            "value": "*",
+                            "type": "function",
                             "alias": None,
-                            "type": "wildcard"
+                            "value": "ST_HISTOGRAM",
+                            "arguments": [
+                                {
+                                    "value": "rast",
+                                    "type": "literal"
+                                },
+                                {
+                                    "value": "elevation",
+                                    "type": "literal"
+                                },
+                                {
+                                    "value": 10,
+                                    "type": "number"
+                                },
+                                {
+                                    "value": "true",
+                                    "type": "literal"
+                                }
+                            ]
                         }
                     ],
-                    "from": "srtm90_v4",
-                    "limit": 1
+                    "from": "CGIAR/SRTM90_V4"
                 }
             }
         }
@@ -183,9 +250,18 @@ def test_get_fields_for_dataset_happy_case(client, mocker):
 
     mocker.get('{}/v1/dataset/bar'.format(os.getenv('CT_URL')), json=dataset_json)
 
-    mocker.get('{}/v1/convert/sql2SQL?sql=SELECT%20*%20FROM%20%22srtm90_v4%22%20LIMIT%201'.format(os.getenv('CT_URL')),
-               json=query_json)
+    mocker.get(
+        '{}/v1/convert/sql2SQL?sql=SELECT%20ST_HISTOGRAM%28rast%2C%20elevation%2C%2010%2C%20true%29%20FROM%20CGIAR%2FSRTM90_V4'.format(os.getenv('CT_URL')),
+        json=download_json)
 
-    response = client.get('/api/v1/earthengine/fields/bar')
+    response = client.get(
+        '/api/v1/earthengine/download/bar?sql=SELECT%20ST_HISTOGRAM%28rast%2C%20elevation%2C%2010%2C%20true%29%20FROM%20CGIAR%2FSRTM90_V4')
+
     assert response.status_code == 200
-    assert response.data == b'{"data":{"fields":{"bands":[{"dataType":{"precision":"INT","range":{"max":32767,"min":-32768}},"grid":{"affineTransform":{"scaleX":0.000833333333333,"scaleY":-0.000833333333333,"translateX":-180,"translateY":60},"crsCode":"EPSG:4326","dimensions":{"height":144000,"width":432000}},"id":"elevation","pyramidingPolicy":"MEAN"}],"endTime":"2000-02-22T00:00:00Z","geometry":{"coordinates":[[[-180,-59.999999999951996],[179.99999999985602,-59.999999999951996],[179.99999999985602,60],[-180,60],[-180,-59.999999999951996]]],"type":"Polygon"},"id":"srtm90_v4","name":"projects/earthengine-public/assets/srtm90_v4","sizeBytes":"18827626666","startTime":"2000-02-11T00:00:00Z","type":"IMAGE","updateTime":"2017-05-08T19:32:14.303Z"},"tableName":"srtm90_v4"}}\n'
+
+    json_response = json.loads(response.data)
+    assert "data" in json_response
+    assert "data" in json_response['data']
+    assert "st_histogram" in json_response['data']['data'][0]
+    assert "elevation" in json_response['data']['data'][0]['st_histogram']
+    assert len(json_response['data']['data'][0]['st_histogram']['elevation']) == 10
